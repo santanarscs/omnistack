cb03a9a8f1ba959809d402a5a81707c2ad9810bb
santanarscs <raphaelstn@gmail.com>
2019-06-21 18:18:02 -0300

    cadastrando usuario

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -14,6 +14,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 Route.post('sessions', 'SessionController.store')
+Route.post('users', 'UserController.store')
 
 Route.group(() => {
   Route.resource('teams', 'TeamController').apiOnly()
