f88345a707cff1aad0cdc50c0e166a6cb9dc246a
santanarscs <raphaelstn@gmail.com>
2019-06-22 19:50:12 -0300

    configurando global style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Provider } from 'react-redux';
 import Routes from './routes';
 import store from './store';
 
+import GlobalSytles from './styles/global';
+
 function App() {
   return (
     <Provider store={store}>
-      <Routes />
+      <Fragment>
+        <GlobalSytles />
+        <Routes />
+      </Fragment>
     </Provider>
   );
 }
