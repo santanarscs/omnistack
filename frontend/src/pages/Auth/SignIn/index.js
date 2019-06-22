4afd3108c9eaa234aa460df17b7724879b1b272b
santanarscs <raphaelstn@gmail.com>
2019-06-22 20:10:58 -0300

    estilizando pagina de login

--- frontend/src/pages/Auth/SignIn/index.js
+++ frontend/src/pages/Auth/SignIn/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, SignForm } from '../styles';
+import Button from '../../../styles/components/Button';
 
 export default function SignIn() {
-  return <div />;
+  return (
+    <Container>
+      <SignForm onSubmit={() => {}}>
+        <h1>Boas vindas</h1>
+        <span>E-MAIL</span>
+        <input type="email" name="email" />
+        <span>SENHA</span>
+        <input type="password" name="password" />
+        <Button size="big" type="submit">
+          Entrar
+        </Button>
+      </SignForm>
+    </Container>
+  );
 }
