d9cd929815686db58744d39b4e1547ac9c9684eb
santanarscs <raphaelstn@gmail.com>
2019-06-27 05:31:17 -0300

    buscando membros

--- frontend/src/components/Members/index.js
+++ frontend/src/components/Members/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -9,27 +9,52 @@ import Modal from '../Modal';
 import Button from '../../styles/components/Button';
 import { MembersList } from './styles';
 
-function Members({ closeMembersModal }) {
-  return (
-    <Modal size="big">
-      <h1>Membros</h1>
+class Members extends Component {
+  static propTypes = {
+    closeMembersModal: PropTypes.func.isRequired,
+    getMembersRequest: PropTypes.func.isRequired,
+    members: PropTypes.shape({
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          name: PropTypes.string,
+        }),
+      ),
+    }).isRequired,
+  };
 
-      <form onSubmit={() => {}}>
-        <MembersList>
-          <li>
-            <strong>Raphael Santana</strong>
-          </li>
-        </MembersList>
-        <Button onClick={closeMembersModal} filled={false} color="gray">
-          Cancelar
-        </Button>
-      </form>
-    </Modal>
-  );
+  componentDidMount() {
+    const { getMembersRequest } = this.props;
+    getMembersRequest();
+  }
+
+  render() {
+    const { closeMembersModal, members } = this.props;
+    return (
+      <Modal size="big">
+        <h1>Membros</h1>
+
+        <form onSubmit={() => {}}>
+          <MembersList>
+            {members.data.map(member => (
+              <li key={member.id}>
+                <strong>{member.name}</strong>
+              </li>
+            ))}
+          </MembersList>
+          <Button onClick={closeMembersModal} filled={false} color="gray">
+            Cancelar
+          </Button>
+        </form>
+      </Modal>
+    );
+  }
 }
-
+const mapStateToProps = state => ({
+  members: state.members,
+});
 const mapDispatchToProps = dispatch => bindActionCreators(MembersActions, dispatch);
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(Members);
